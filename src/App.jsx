7a97a7e324d4da6b237135e016c5814a145063c0
nomanmajeed1 <nomanmian7860@gmail.com>
2024-07-04 15:49:59 +0500

    data
    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import {useState} from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-// api key:= a93f5a1d
-function App() {
-  const [count, setCount] = useState(0);
 
+function App() {
   return (
     <>
       <h1>hello</h1>
+      <h1>hello noman majeed hello</h1>
     </>
   );
 }
---
+++ src/Redux/RootSagas.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RootSagas = () => {
+  return <div>RootSagas</div>;
+};
+
+export default RootSagas;
