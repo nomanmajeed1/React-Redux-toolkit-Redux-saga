b6e261e3d782ce56ea6ee649934417d8ed73e37a
nomanmajeed1 <seebiz.bpt0623.dev22@gmail.com>
2024-07-04 13:59:48 +0500

    Component file and redux file

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {useState} from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+// api key:= a93f5a1d
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default App;
